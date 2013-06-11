c6d674a6d9c2ba9e90182dad0e646e8b833fb923
Aron Hegyi <ahegyi@gmail.com>
2013-06-10 19:38:21 -0700

    Move AWS region and S3 bucket to environment variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region                 => 'us-east-1'                  # optional, defaults to 'us-east-1'
+    :region                 => ENV['AWS_REGION']   # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'reciplease'                     # required
+  config.fog_directory  = ENV['AWS_S3_BUCKET']   # required
 end
